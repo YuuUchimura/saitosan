b9c551ec6f84f78f26efd95f479ad7881917b012
Yuu <anpanmamZ3624@gmail.com>
2022-01-16 00:45:04 +0900

    finish

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+const setBtn = () => {
+    const num = Math.random() * 10;
+    // if(num < 5){
+    //     console.log("ぺっぺっぺー")
+    // } else {
+    //     console.log("斎藤さんだぞぉぉぉぉ！！")
+    // }
+    // // console.log(num)
+    num < 5 ?
+        confirm("ぺっぺっぺー"):
+        confirm("斎藤さんだぞぉぉぉぉ！！")
+}
